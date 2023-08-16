35de89a0e1f009ed5cce737c561888c0924e7939
Rafael Pacheco <rafaelsoaresp@hotmail.com>
2023-08-16 12:59:18 -0300

    16/08

---
+++ ORIGAMID/JS/exerciciosDom/buttonCreator/script.js
@@ -0,0 +1,49 @@
+let controles = document.getElementById('controles')
+let cssT = document.querySelector('.css')
+let btn = document.querySelector('.btn')
+
+controles.addEventListener('change', handleChange)
+
+let handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  color(value) {
+    this.element.style.color = value
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'rem'
+  },
+  texto(value) {
+    this.element.innerText = value;
+  }
+
+}
+
+function handleChange(event) {
+  let name = event.target.name
+  let value = event.target.value
+
+  handleStyle[name](value)
+  showCss()
+}
+
+function showCss () {
+  cssT.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
+}
